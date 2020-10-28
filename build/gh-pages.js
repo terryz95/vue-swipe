669990b357ca3167444d46428b7a09d2bef1ce71
Terry Zhang <mousetrip@outlook.com>
2020-10-28 17:49:16 +0800

    feat: 案例优化 & 案例发布脚本更新

--- build/gh-pages.js
+++ build/gh-pages.js
@@ -1,7 +1,8 @@
 const ghpages = require('gh-pages')
+const colors = require('colors')
 const version = require('../package.json').version
 
-ghpages.publish('examples', {message: `[${version}] examples update`}, function(err) {
+ghpages.publish('examples', {message: `[${version}] examples update - ${new Date().toLocaleString("en-US", {hour12: false})}`}, function(err) {
   if (err) {
     console.log(colors.bgRed(err.toString()))
   } else {
